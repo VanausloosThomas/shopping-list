7be64ec452c544435505e4ae284364d26b4c9d64
VanausloosThomas <thomas.vanausloos@cegeka.com git config --global core.autocrlf inputgit config --global user.email thomas.vanausloos@cegeka.com>
2017-12-30 21:13:25 +0100

    added UserRepository

---
+++ shopping-list/src/main/java/com/thomas/shoppinglist/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.thomas.shoppinglist.repositories;
+
+import com.thomas.shoppinglist.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Override
+    Optional<User> findById(String s);
+
+}
---
+++ shopping-list/src/test/java/com/thomas/shoppinglist/repositories/UserRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.thomas.shoppinglist.repositories;
+
+import com.thomas.shoppinglist.ShoppingListApplication;
+import com.thomas.shoppinglist.users.User;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.inject.Inject;
+
+import static com.thomas.shoppinglist.users.User.UserBuilder.userBuilder;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShoppingListApplication.class, webEnvironment = RANDOM_PORT)
+public class UserRepositoryTest {
+
+    @Inject
+    private UserRepository userRepository;
+    @Test
+    public void save_whenSavingAUser_thenUserGetsPersisted() {
+        User userToPersist = userBuilder()
+                .withFirstName("Thomas")
+                .withLastName("Vanausloos")
+                .withUsername("ThomasVanausoos")
+                .withPassword("password")
+                .withEmail("thomasvanausloos@gmail.com")
+                .build();
+
+        userRepository.save(userToPersist);
+
+        Assertions.assertThat(userRepository.findById(userToPersist.getId())).isPresent();
+    }
+}
